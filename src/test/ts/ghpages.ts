577aa9859eb66222eb092a1fc59f32f30fbbc959
Anton Golub <mailbox@antongolub.ru>
2020-04-25 20:56:56 +0300

    test: add separate test for pullTags action

---
+++ src/test/ts/ghpages.ts
@@ -0,0 +1,72 @@
+import { IPushOpts, TAnyMap } from '../../main/ts/interface'
+
+describe('ghpages', () => {
+  const fakeExeca = jest.fn(() => Promise.resolve())
+  let pullTags: any
+
+  beforeAll(() => {
+    jest.resetModules()
+    jest.mock('execa', () => fakeExeca)
+    jest.mock('gh-pages', () => ({
+      clean: () => { /* noop */ },
+      publish: jest.fn((_src: string, _opts: TAnyMap, cb: Function) => cb())
+    }))
+
+    pullTags = require('../../main/ts/ghpages').pullTags
+  })
+
+  afterEach(fakeExeca.mockClear)
+
+  afterAll(() => {
+    jest.unmock('gh-pages')
+    jest.unmock('execa')
+    jest.resetModules()
+  })
+
+  const pushOptsStub = {
+    message: 'stub',
+    logger: console,
+    env: {},
+    repo: 'repo',
+    cwd: 'stub',
+    src: 'stub',
+    dst: 'stub',
+    branch: 'stub',
+    msg: 'stub'
+  }
+
+  describe('#pullTags', () => {
+    it('does nothing if `opts.pullTagsBranch === \'\'`', async () => {
+      const opts: IPushOpts = {
+        ...pushOptsStub,
+        pullTagsBranch: ''
+      }
+      expect(await pullTags(opts)).toBeUndefined()
+      expect(fakeExeca).not.toHaveBeenCalled()
+    })
+
+    it('invokes execa with proper args', async () => {
+      const opts: IPushOpts = {
+        ...pushOptsStub,
+        pullTagsBranch: 'foo'
+      }
+      const execaOpts = {
+        cwd: opts.cwd,
+        env: opts.env
+      }
+
+      await pullTags(opts)
+      expect(fakeExeca).toHaveBeenCalledWith(
+        'git',
+        [
+          'pull',
+          '--tags',
+          '--force',
+          opts.repo,
+          opts.pullTagsBranch
+        ],
+        execaOpts
+      )
+    })
+  })
+})
